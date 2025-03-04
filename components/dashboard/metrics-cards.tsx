8432f6f29575e5b2da2be964675c4d90c9a9374e
tushar.base.eth <tushar.base.eth@gmail.com>
2025-03-05 00:30:27 +0700

    dashboard unit formating

--- components/dashboard/metrics-cards.tsx
+++ components/dashboard/metrics-cards.tsx
@@ -6,13 +6,13 @@ interface MetricsCardsProps {
   totalVolume: number;
 }
 
-const formatNumber = (num: number): string => {
+const formatNumber = (num: number, decimals: number = 2): string => {
   if (num >= 1000000) {
-    return (num / 1000000).toFixed(0) + "M";
+    return (num / 1000000).toFixed(1) + "M";
   } else if (num >= 1000) {
-    return (num / 1000).toFixed(0) + "K";
+    return (num / 1000).toFixed(1) + "K";
   }
-  return num.toString();
+  return num.toFixed(decimals);
 };
 
 export function MetricsCards({ totalWorkouts, totalVolume }: MetricsCardsProps) {
