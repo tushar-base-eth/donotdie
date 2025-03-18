66abdaac0d224811bb0e723518fc944aba62012a
tushar.base.eth <tushar.base.eth@gmail.com>
2025-03-19 01:57:56 +0700

    apple-mobile-web-app-capable is deprecated

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.webmanifest" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
